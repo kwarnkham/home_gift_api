aa0f2570d0fa47487a9c0581a78b1ddd8c9ff7e7
kwarnkham <kwarnkham.moon@gmail.com>
2019-08-05 10:43:43 +0630

    created migrations

---
+++ database/migrations/2019_08_05_035606_add_merchant_id_and_location_id_to_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMerchantIdAndLocationIdToItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->unsignedBigInteger('merchant_id');
+            $table->unsignedBigInteger('location_id');
+            $table->foreign('merchant_id')->references('id')->on('merchants');
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            //
+        });
+    }
+}
