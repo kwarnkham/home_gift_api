527dd4666a157757251fa33df31c8d575d7807f0
SAI KWRN KHAM <kwarnkham.moon@gmail.com>
2019-09-24 17:50:18 +0630

    update migration to include merchant and location id

--- database/migrations/2019_09_24_070134_create_item_order_table.php
+++ database/migrations/2019_09_24_070134_create_item_order_table.php
@@ -23,9 +23,13 @@ class CreateItemOrderTable extends Migration
             $table->text('description');
             $table->text('notice');
             $table->double('weight');
+            $table->unsignedBigInteger('location_id');
+            $table->unsignedBigInteger('merchant_id');
             $table->timestamps();
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade'); 
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+            $table->foreign('merchant_id')->references('id')->on('merchants')->onDelete('cascade'); 
         });
     }
 
