827c0ee47e971479c9a0c16a4c941ef72fc471c4
SAI KWRN KHAM <kwarnkham.moon@gmail.com>
2019-09-24 14:06:19 +0630

    more migrations

---
+++ database/migrations/2019_09_24_070134_create_item_order_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_order', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('item_id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->text('description');
+            $table->text('notice');
+            $table->double('weight');
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_order');
+    }
+}
