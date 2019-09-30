3d19ee1268ca1c70ba49313ffde89b7c4121f063
SAI KWRN KHAM <kwarnkham.moon@gmail.com>
2019-09-30 17:45:31 +0630

    promotion status to table

--- database/migrations/2019_09_23_000000_create_promotions_table.php
+++ database/migrations/2019_09_23_000000_create_promotions_table.php
@@ -18,6 +18,7 @@ class CreatePromotionsTable extends Migration
             $table->string('name');
             $table->integer('amount');
             $table->text('note');
+            $table->boolean('active')->default(0);
             $table->timestamps();
         });
     }
