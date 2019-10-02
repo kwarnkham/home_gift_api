ec88dbf7c37969b20a300e57bf9ea8cf4b7c4696
kwarnkham <kwarnkham.moon@gmail.com>
2019-10-02 15:43:23 +0630

    test

--- routes/api.php
+++ routes/api.php
@@ -2,27 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-/*
-|--------------------------------------------------------------------------
-| API return code infomation
-|--------------------------------------------------------------------------
-|
-| 0 - success
-| 1 - handled issue
-| 2 - token issue
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
