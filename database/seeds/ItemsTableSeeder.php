f2f93d86581f74823c4452bee78533d216ecaa2c
kwarnkham <kwarnkham.moon@gmail.com>
2020-02-05 18:11:48 +0630

    more seeding

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -36,5 +36,23 @@ class ItemsTableSeeder extends Seeder
             ]);
             $temp->categories()->attach(1);
         }
+        foreach ($locations as $location) {
+            $temp = App\Item::create([
+                'name' => "More item from " . $location->name,
+                'ch_name' => "More item from " . $location->name,
+                'mm_name' => "More item from " . $location->name,
+                'price' => rand(1000, 10000),
+                'description' => 'Description of item from ' . $location->name . ' in full information and details',
+                'ch_description' => 'Chinese Description of item from ' . $location->name . ' in full information and details',
+                'mm_description' => 'Myanmar Description of item from ' . $location->name . ' in full information and details',
+                'notice' => 'Notice of item from ' . $location->name . ' if there is',
+                'ch_notice' => 'Chinese Notice of item from ' . $location->name . ' if there is',
+                'mm_notice' => 'Chinese Notice of item from ' . $location->name . ' if there is',
+                'weight' => rand(1, 10) . "kg",
+                'location_id' => $location->id,
+                'merchant_id' => rand(1, 6)
+            ]);
+            $temp->categories()->attach(1);
+        }
     }
 }
