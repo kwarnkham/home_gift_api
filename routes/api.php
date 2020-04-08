2b5429de675bde4a21092be84d97585fa9e5bf64
Moon <kwarnkham.moon@gmail.com>
2020-04-08 17:08:04 +0630

    manage admin middleware

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,43 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware(['auth:api', 'checkAdmin'])->group(function () {
+    Route::put('/delivery-fees', 'DeliveryFeesController@update');
 
+    Route::post('/item/{id}/{categoryId}', 'ItemController@addCategory');
+    Route::put('/item/{id}/categories', 'ItemController@updateCategory');
+    Route::patch('item/{id}', 'ItemController@unDestroy');
+    Route::put('/item/{id}', 'ItemController@update');
+    Route::delete('/item/{id}', 'ItemController@destroy');
+    Route::post('/item', 'ItemController@store');
+
+    Route::put('/merchant', 'MerchantController@update');
+    Route::post('/merchant', 'MerchantController@store');
+
+    Route::post('/location', 'LocationController@store');
+    Route::put('/location', 'LocationController@update');
+    Route::post('/province', 'ProvinceController@store');
+    Route::get('/provinces', 'ProvinceController@index');
+
+    Route::put('/category/make-a/{id}', 'CategoryController@makeCategoryA');
+    Route::put("/category/unmake-a/{id}", 'CategoryController@unMakeCategoryA');
+    Route::post('/category', 'CategoryController@store');
+    Route::put('/category', 'CategoryController@update');
+    Route::post('/category/make-b/{id}', 'CategoryController@makeCategoryB');
+    Route::delete('b-category/{id}', 'CategoryController@destroyCategoryB');
+    Route::post('/category/join-ab/{aId}/{bId}', 'CategoryController@joinAB');
+    Route::delete('category/unjoin-ab/{bId}', 'CategoryController@unJoinAB');
+    Route::post('category/join-bc/{bId}/{id}', 'CategoryController@joinBC');
+    Route::delete('category/unjoin-bc/{id}', 'CategoryController@unJoinBC');
+
+    Route::post('/image', 'ImageController@store');
+    Route::delete('/image/{id}', 'ImageController@destroy')->where('id', '[0-9]+');
+
+    Route::post('/order/action/{id}', 'OrderController@update')->where('id', '[0-9]+');
+});
 Route::get('/delivery-fees', 'DeliveryFeesController@find');
 Route::get('/all-delivery-fees', 'DeliveryFeesController@index');
-Route::put('/delivery-fees', 'DeliveryFeesController@update');
+
 Route::get('/items', 'ItemController@index');
 Route::get('/item/{id}', 'ItemController@find');
 Route::get('/items/trashed', 'ItemController@indexTrashed');
@@ -17,56 +47,19 @@ Route::get('/items/find/category/{categoryId}', 'ItemController@findByCategory')
 Route::get('/items/find/location/{locationId}', 'ItemController@findByLocation');
 Route::get('/items/find/merchant/{merchantId}', 'ItemController@findByMerchant');
 Route::get('/items/find/province/{provinceId}', 'ItemController@findByProvince');
-// Route::get('/items/find/{}')
 
-Route::post('/item', 'ItemController@store');
-Route::post('/item/{id}/{categoryId}', 'ItemController@addCategory');
-Route::put('/item/{id}/categories', 'ItemController@updateCategory');
-Route::patch('item/{id}', 'ItemController@unDestroy');
-Route::put('/item/{id}', 'ItemController@update');
-Route::delete('/item/{id}', 'ItemController@destroy');
-
-Route::post('/merchant', 'MerchantController@store');
 Route::get('/merchants', 'MerchantController@index');
-Route::put('/merchant', 'MerchantController@update');
-
-Route::post('/location', 'LocationController@store');
 Route::get('/locations', 'LocationController@index');
-Route::put('/location', 'LocationController@update')->middleware('auth:api', 'checkAdmin');
-
-Route::post('/province', 'ProvinceController@store');
-Route::get('/provinces', 'ProvinceController@index');
-// Route::put('/location', 'LocationController@update');
-
-Route::post('/category', 'CategoryController@store');
 Route::get('/categories', 'CategoryController@index');
-Route::put('/category', 'CategoryController@update');
-
-Route::middleware('throttle:500,1')->group(function () {
-    Route::put('/category/make-a/{id}', 'CategoryController@makeCategoryA');
-    Route::get('/categories/get-a', 'CategoryController@indexCategoryA');
-    Route::put("/category/unmake-a/{id}", 'CategoryController@unMakeCategoryA');
-
-    Route::get("/categories/get-b", 'CategoryController@indexCategoryB');
-    Route::get("/categories/get-b/{id}", 'CategoryController@getBCategoriesOfA');
-    Route::post('/category/make-b/{id}', 'CategoryController@makeCategoryB');
-    Route::delete('b-category/{id}', 'CategoryController@destroyCategoryB');
-    Route::post('/category/join-ab/{aId}/{bId}', 'CategoryController@joinAB');
-    Route::get('/category/joinedA/{bId}', 'CategoryController@getJoinedA');
-    Route::get('/category/AB', 'CategoryController@getAB');
-    Route::delete('category/unjoin-ab/{bId}', 'CategoryController@unJoinAB');
-
-    Route::get("/categories/get-c/{id}", 'CategoryController@getCCategoriesOfB');
-    Route::post('category/join-bc/{bId}/{id}', 'CategoryController@joinBC');
-    Route::get('/category/BC', 'CategoryController@getBC');
-    Route::get('/category/joinedB/{id}', 'CategoryController@getJoinedB');
-    Route::delete('category/unjoin-bc/{id}', 'CategoryController@unJoinBC');
-});
-
-
-Route::post('/image', 'ImageController@store');
-Route::delete('/image/{id}', 'ImageController@destroy')->where('id', '[0-9]+');
-
+Route::get('/categories/get-a', 'CategoryController@indexCategoryA');
+Route::get("/categories/get-b", 'CategoryController@indexCategoryB');
+Route::get("/categories/get-b/{id}", 'CategoryController@getBCategoriesOfA');
+Route::get('/category/joinedA/{bId}', 'CategoryController@getJoinedA');
+Route::get('/category/AB', 'CategoryController@getAB');
+Route::get('/category/BC', 'CategoryController@getBC');
+Route::get('/category/joinedB/{id}', 'CategoryController@getJoinedB');
+Route::get("/categories/get-c/{id}", 'CategoryController@getCCategoriesOfB');
+    
 Route::post('/user', 'UserController@store');
 Route::post('/login', 'UserController@login');
 Route::post('/logout', 'UserController@logout')->middleware('auth:api');
@@ -78,4 +71,3 @@ Route::put('/user', 'UserController@update')->middleware('auth:api');
 Route::post('/order', 'OrderController@store')->middleware('auth:api');
 Route::get('/order/user', 'OrderController@userOrder')->middleware('auth:api');
 Route::get('/orders', 'OrderController@index');
-Route::post('/order/action/{id}', 'OrderController@update')->where('id', '[0-9]+');
