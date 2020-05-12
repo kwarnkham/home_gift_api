2c41b588671326a4071e154037d089694b81b36e
Moon <kwarnkham.moon@gmail.com>
2020-05-12 16:34:02 +0630

    separate township

--- database/migrations/2020_05_12_052922_create_townships_table.php
+++ database/migrations/2020_05_12_052922_create_townships_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAddressesTable extends Migration
+class CreateTownshipsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateAddressesTable extends Migration
      */
     public function up()
     {
-        Schema::create('addresses', function (Blueprint $table) {
+        Schema::create('townships', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('city_id');
-            $table->string('township_id');
-            $table->string('address');
+            $table->string('name');
+            $table->unsignedBigInteger('city_id');
             $table->timestamps();
             $table->foreign('city_id')->references('id')->on('city');
-            $table->foreign('township_id')->references('id')->on('township');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateAddressesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('addresses');
+        Schema::dropIfExists('townships');
     }
 }
--- database/migrations/2020_05_12_052923_create_cities_table.php
+++ database/migrations/2020_05_12_052923_create_cities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAddressesTable extends Migration
+class CreateCitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateAddressesTable extends Migration
      */
     public function up()
     {
-        Schema::create('addresses', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('city_id');
-            $table->string('township_id');
-            $table->string('address');
+            $table->string('name');
             $table->timestamps();
-            $table->foreign('city_id')->references('id')->on('city');
-            $table->foreign('township_id')->references('id')->on('township');
         });
     }
 
@@ -31,6 +27,6 @@ class CreateAddressesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('addresses');
+        Schema::dropIfExists('cities');
     }
 }
