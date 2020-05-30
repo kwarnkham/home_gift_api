1cc90f24cd2f20dc0f212726399fed7daf1261a1
kwarnkham <kwarnkham.moon@gmail.com>
2020-05-30 13:27:22 +0630

    ios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -289,6 +289,6 @@ class UserController extends Controller
 
     public function getVersion(Request $request)
     {
-        return ['code' => '0', 'msg' => 'ok', 'result' => ['version' => ['android' => '0.0.1', 'ios' => '']]];
+        return ['code' => '0', 'msg' => 'ok', 'result' => ['version' => ['android' => '0.0.1', 'ios' => '0.0.1']]];
     }
 }
