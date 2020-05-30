4301d037e9264dc40d2ae96ea50f290564ac9dd4
kwarnkham <kwarnkham.moon@gmail.com>
2020-05-30 13:15:59 +0630

    upgrade android version

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -289,6 +289,6 @@ class UserController extends Controller
 
     public function getVersion(Request $request)
     {
-        return ['code' => '0', 'msg' => 'ok', 'result' => ['version' => '1.0']];
+        return ['code' => '0', 'msg' => 'ok', 'result' => ['version' => ['android' => '0.0.1', 'ios' => '']]];
     }
 }
